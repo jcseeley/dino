54a5384b299ea8b784d5c108d1deae5a4274efb5
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-08 13:38:04 -0800

    add hangman function.

---
+++ src/js/hangman.js
@@ -0,0 +1,45 @@
+let myArray = ["d", "i", "n", "o", "s", "a", "u", "r", "u", "s"]
+let newArray = [];
+myArray.forEach((letter, i) => {
+  if (myArray[i].includes("s")) {
+    newArray.push("s");
+  } else {
+  newArray.push("_")
+  }
+})
+console.log(newArray);
+
+
+
+// let myArray = ["d", "i", "n", "o", "s", "a", "u", "r", "u", "s"]
+// let newArray = [];
+// myArray.forEach(() => {
+//   newArray.push("_");
+// })
+// console.log(newArray);
+
+
+
+// myDino.forEach(function(letter) {
+//   $(".blankSpaces").append('<li>' + "_ " + '</li>'); 
+//   myArray.push("_");
+//   dinoLetters.push(letter);
+
+
+// submit guess 
+// if myDino.includes(input.val) and myArray[i] === "_", myDino.replace(input.val)
+//   appear method at letter location  
+
+// let myVar = input.val
+
+// let regex = '/${myVar}/gi'
+
+// const arr = ['a', 'b', 'c', 'a', 'a'];
+
+// for (let index = 0; index < myDino.length; index++) {
+//   if (arr[index] === 'a') {
+//     arr[index] = 'z';
+//   }
+// }
+
+// console.log(arr); // 👉️ ['z', 'b', 'c']
